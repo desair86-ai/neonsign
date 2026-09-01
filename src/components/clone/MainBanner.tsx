1b9d0aadbe41b442439b9941fa76bf72a8b7bc0b
theinspiringlifecommunities-ops <archcart129@gmail.com>
2026-09-01 15:16:54 +0530

    Fix mobile banner crop

--- src/components/clone/MainBanner.tsx
+++ src/components/clone/MainBanner.tsx
@@ -6,7 +6,7 @@ export function MainBanner() {
     <div className="relative w-full overflow-hidden">
       <Link href="/products/customize-neon-signs" className="block relative w-full h-[50vh] sm:h-[60vh] md:h-[70vh] xl:h-[80vh] min-h-[400px]">
         <div 
-          className="absolute inset-0 bg-cover bg-center bg-no-repeat w-full h-full"
+          className="absolute inset-0 bg-cover bg-left md:bg-center bg-no-repeat w-full h-full"
           style={{ backgroundImage: "url('/Website%20Banner-02.png')" }}
         />
       </Link>
